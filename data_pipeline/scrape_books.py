f82b25b87a91035f4c904701c64aaff401ec7de8
jaya-python-cpu <jayapedapenki@gmail.com>
2026-09-22 14:57:38 +0530

    Update scrape_books.py

--- data_pipeline/scrape_books.py
+++ data_pipeline/scrape_books.py
@@ -3,6 +3,7 @@ from bs4 import beautifulsoup
 
 url= "https://books.toscrape.com/"
 response=requests.get(url)
+response.raise_for_satus()
 
 soup= Beautifulsoup(response.text, "html.parser")
 print(soup.title.text)
